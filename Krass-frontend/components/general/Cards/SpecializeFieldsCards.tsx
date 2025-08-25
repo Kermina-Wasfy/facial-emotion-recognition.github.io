bfe2e49f966b389068eb4ab0b0515c7f26571011
Kermina Wasfy <wkermina@gmail.com>
2025-08-25 14:07:55 +0300

    second commit for home page

--- Krass-frontend/components/general/Cards/SpecializeFieldsCards.tsx
+++ Krass-frontend/components/general/Cards/SpecializeFieldsCards.tsx
@@ -9,23 +9,23 @@ interface ServiceCardProps {
 const ServiceCard: React.FC<ServiceCardProps> = ({ icon, title, description }) => {
   return (
     <div className="bg-[#252424] border border-gray-400 rounded-md py-6 pl-6 flex flex-col gap-4 text-white hover:border-white transition relative cursor-pointer">
-      {/* L Shape */}
+      {/* Icon border */}
       <div className="border-gray-400 absolute top-4 left-4 w-16 h-16 border-t-1 border-l-1"></div>
 
       {/* Icon + Title Row */}
       <div className="flex items-center relative">
-        {/* Icon on the left */}
+        {/* Icon */}
         <div className="w-16 h-16">
           <img src={icon} alt={title} className="w-full h-full object-contain" />
         </div>
 
-        {/* Title centered absolutely */}
+        {/* Title */}
         <h3 className="absolute left-1/2 transform -translate-x-1/2 text-xl font-semibold whitespace-nowrap">
           {title}
         </h3>
       </div>
 
-      {/* Line aligned to the right */}
+      {/* Divider */}
       <hr className="border-gray-400 self-end w-2/3" />
 
       {/* Description */}
